d827810160be669a2d4a27fdb4c495b450b42d9b
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-04-10 15:52:38 -0600

    User logout implemented.

--- src/app/features/admin/components/admin-sidebar/admin-sidebar.ts
+++ src/app/features/admin/components/admin-sidebar/admin-sidebar.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { AuthApi } from '@core/services/auth-api';
 import { GlobalFooter } from "@shared/components/global-footer/global-footer";
 
 @Component({
@@ -9,11 +10,19 @@ import { GlobalFooter } from "@shared/components/global-footer/global-footer";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdminSidebar {
+  // Init
   protected navigationLinks = [
     { title: 'Usuarios', link: 'admin/usuarios', imageUrl: 'assets/icons/user-icon.svg' },
     { title: 'Galería', link: 'admin/galeria', imageUrl: 'assets/icons/image-icon.svg' },
     { title: 'Noticias', link: 'admin/noticias', imageUrl: 'assets/icons/new-icon.svg' },
     { title: 'Documentos', link: 'admin/documentos', imageUrl: 'assets/icons/document-icon.svg' },
     { title: 'Configuraciones', link: 'admin/configuraciones', imageUrl: 'assets/icons/settings-icon.svg' },
-  ]
+  ];
+
+  // Inject
+  private authApiService = inject(AuthApi);
+
+  protected onLogout() {
+    this.authApiService.logoutUser();
+  }
 }
