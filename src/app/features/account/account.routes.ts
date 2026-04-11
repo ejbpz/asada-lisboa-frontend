957b15b146929654958100bca958066efd7db31f
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-04-10 19:47:42 -0600

    Adding guard to avoid login when is authenticated.

--- src/app/features/account/account.routes.ts
+++ src/app/features/account/account.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
+import { nonAuthenticatedUserGuard } from "@core/guards/non-authenticated-user-guard";
 
 const authRoutes: Routes = [
   {
     path: 'iniciar-sesion',
-    loadComponent: () => import('@account/pages/login-page/login-page')
+    loadComponent: () => import('@account/pages/login-page/login-page'),
+    canActivate: [nonAuthenticatedUserGuard]
   },
   // {
   //   path: 'olvido-contrasena',
