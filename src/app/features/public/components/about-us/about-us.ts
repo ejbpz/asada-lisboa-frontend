fe88ca4116e49afc442597e922c9c2180642de7f
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-15 16:21:12 -0600

    Add about-us page in features public

---
+++ src/app/features/public/components/about-us/about-us.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  imports: [],
+  templateUrl: './about-us.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutUs { }
