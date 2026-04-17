ee2d9a5dd2906c66acf681ad8383b5d59257841a
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-17 09:24:28 -0600

    Create public-directors-board-section

---
+++ src/app/features/public/components/ public-directors-board-section/ public-directors-board-section.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app--public-directors-board-section',
+  imports: [],
+  templateUrl: './ public-directors-board-section.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PublicDirectorsBoardSection { }
