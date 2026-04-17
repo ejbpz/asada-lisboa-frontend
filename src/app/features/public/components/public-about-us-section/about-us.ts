88693fac35ff671e737044bde679c0144fdf77eb
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-17 17:12:51 -0600

    Modified about-us ts

--- src/app/features/public/components/public-about-us-section/about-us.ts
+++ src/app/features/public/components/public-about-us-section/about-us.ts
@@ -1,11 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, inject, signal  } from '@angular/core';
+import { AboutUsApi } from '@core/services/about-us-api';
+import { AboutUsResponse} from '@public/interfaces/about-us-response.interface';
 
 @Component({
-  selector: 'app-about-us',
+  selector: 'about-us',
   imports: [],
   templateUrl: './about-us.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutUs {
+export class AboutUs implements AfterViewInit{
+// Init
+  protected isLoading = signal<boolean>(false);
+  protected aboutUsData = signal<AboutUsResponse[] | null>(null);
+
+  // Inject
+  protected aboutUsService = inject(AboutUsApi);
+
+  // AfterViewInit
+  ngAfterViewInit(): void {
+    this.principalApiService();
+  }
+
+ // Calling principal API
+  protected principalApiService(): void {
+      if(this.isLoading())
+        return;
+
+      this.isLoading.set(true);
+
+      this.aboutUsService.getAboutUsInformation()
+        .subscribe({
+          next: (aboutUSResponse: AboutUsResponse[]) => {
+            this.aboutUsData.set(aboutUSResponse);
+            this.isLoading.set(false);
+          }
+        });
+  }
+
+
+
 
  }
