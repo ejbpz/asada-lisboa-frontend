a038d0151871e7527310904d702a435b156aaebc
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-20 12:57:09 -0600

    Modified public-directors-board-section

--- src/app/features/public/components/ public-directors-board-section/ public-directors-board-section.ts
+++ src/app/features/public/components/ public-directors-board-section/ public-directors-board-section.ts
@@ -1,9 +1,47 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { DirectorsBoardApi } from '@core/services/directors-board-api';
+import { DirectorsBoardResponse } from '@public/interfaces/directors-board-response.interface';
 
 @Component({
-  selector: 'app--public-directors-board-section',
+  selector: 'public-directors-board-section',
   imports: [],
   templateUrl: './ public-directors-board-section.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+   host: {
+    class: 'w-full flex justify-center items-center md:w-1/2'
+  }
 })
-export class PublicDirectorsBoardSection { }
+export class PublicDirectorsBoardSection implements AfterViewInit{
+  
+   // Init
+  protected isLoading = signal<boolean>(false);
+  protected directorBoardData = signal<DirectorsBoardResponse[] | null>(null);
+
+  // Inject
+  protected directorBoardService = inject(DirectorsBoardApi);
+
+  // AfterViewInit
+  ngAfterViewInit(): void {
+    this.principalApiService();
+  }
+
+    // Calling principal API
+  protected principalApiService(): void {
+      if(this.isLoading())
+        return;
+
+      this.isLoading.set(true);
+
+      this.directorBoardService.getDirectorsBoardInformation()
+        .subscribe({
+          next: (directorsBoardResponse: DirectorsBoardResponse[]) => {
+            this.directorBoardData.set(directorsBoardResponse);
+            this.isLoading.set(false);
+          }
+        });
+  }
+
+
+
+
+ }
