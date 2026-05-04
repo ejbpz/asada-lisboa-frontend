3a26699aa81c64787d6caf8650cb1299c64b5530
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-05-04 13:26:46 -0600

    Global date format to UTC-6 (Costa Rica).

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
 import { provideHotToastConfig } from '@ngxpert/hot-toast';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 import { jwtTokenInterceptor } from '@core/interceptors/jwt-token-interceptor';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHotToastConfig(),
     provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([jwtTokenInterceptor])),
-    provideHotToastConfig(),
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: 'dd/MM/yyyy', timezone: '-0600' }
+    }
   ]
 };
