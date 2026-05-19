12f8d0f5ab46288c00182320d0532763b8ef5d82
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-05-19 12:11:07 -0600

    Testing for ChargesApi.

---
+++ src/app/core/services/charges-api.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { ChargesApi } from './charges-api';
+import { environment } from '@environments/environment.development';
+
+describe('ChargesApi', () => {
+  let service: ChargesApi;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
+
+    service = TestBed.inject(ChargesApi);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should get charges', () => {
+    const mockResponse = [
+      {
+        id: '1',
+        name: 'Presidente'
+      },
+      {
+        id: '2',
+        name: 'Secretario'
+      }
+    ];
+
+    service.getCharges()
+      .subscribe(response => {
+        expect(response).toEqual(mockResponse);
+        expect(response.length).toBe(2);
+      });
+
+    const req = httpMock.expectOne(
+      `${environment.API_URL_ADMIN}/cargos`
+    );
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockResponse);
+  });
+});
