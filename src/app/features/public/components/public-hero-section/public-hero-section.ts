7da48dc6525966ac00f5232bc8230b63c78cce00
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-05-22 10:25:49 -0600

    Changing hero section two vertical spaces with an image.

--- src/app/features/public/components/public-hero-section/public-hero-section.ts
+++ src/app/features/public/components/public-hero-section/public-hero-section.ts
@@ -1,8 +1,6 @@
 import { RouterLink } from '@angular/router';
 import { TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, effect, input, signal } from '@angular/core';
-import { GenerateContent } from '@shared/utils/generate-content';
-import { ImageMinimalResponse } from '@public/interfaces/image-minimal-response.interface';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'public-hero-section',
@@ -10,22 +8,10 @@ import { ImageMinimalResponse } from '@public/interfaces/image-minimal-response.
   templateUrl: './public-hero-section.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'flex justify-center py-12.5 lg:py-25 px-4 lg:px-12'
+    class: 'flex justify-center pt-12.5 md:px-12'
   }
 })
 export class PublicHeroSection {
-  // Init
-  protected generateContent = GenerateContent;
-  protected data = signal<ImageMinimalResponse[] | undefined>(undefined);
-
-  // Input signal
-  public images = input.required<ImageMinimalResponse[] | undefined>();
-
-  // Effects
-  protected imageReduced = effect(() => {
-    this.data.set(this.images()?.slice(3));
-  });
-
   // Default values
   protected title: string = 'asada';
   protected subtitle: string = 'urbanización lisboa';
@@ -35,20 +21,4 @@ export class PublicHeroSection {
     link: '/noticias',
     title: 'noticias'
   };
-
-  // Template methods
-  protected getImageClasses(index: number): string {
-    const base = 'rounded-lg shadow-md object-cover w-full h-full';
-
-    switch (index) {
-      case 0:
-        return `${base} row-start-2 row-span-3`;
-      case 1:
-        return `${base} col-start-2 row-span-2`;
-      case 2:
-        return `${base} col-start-2 row-start-3 row-span-3`;
-      default:
-        return base;
-    }
-  }
 }
